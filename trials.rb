37229fabfbeabcb1d4650849d1dd631b5e895971
Liz Abinante <eabinante@gmail.com>
2013-03-14 15:04:23 -0500

    fancy methods

---
+++ trials.rb
@@ -0,0 +1,19 @@
+puts 'Hey, how are you doing today?'
+emote = gets.chomp
+puts 'It\'s always good to hear that someone feels ' + emote + '.'
+puts 'How do you feel about My Little Ponies?'
+pony = gets.chomp
+puts 'What is this "' + pony.upcase + '" bull shit?'
+follow = gets.chomp
+puts '"' + follow.upcase + '"? Absolutely not. You\'re fired.' 
+puts ''
+
+line_width = 90
+puts ('Table of Contents'.center(line_width))
+puts ''
+puts ('Chapter 1: Surviving Ruby'.ljust(line_width/2) + 'page 1'.rjust(line_width/2))
+puts ('Chapter 2: Methods and objects'.ljust(line_width/2) + 'page 81'.rjust(line_width/2))
+puts ('Chapter 3: Pie day'.ljust(line_width/2) + 'page 314'.rjust(line_width/2))
+puts ''
+puts ('The End'.center(line_width))
+puts ''
