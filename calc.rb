e538c1ac22d6e6ca473c5da94b002c1414664936
Liz Abinante <eabinante@gmail.com>
2013-03-14 10:42:10 -0500

    strings and floats

--- calc.rb
+++ calc.rb
@@ -2,3 +2,15 @@ puts 365*24
 puts (24*60)*(365*10)
 puts (24*60*60)*(365*27)
 puts (851472000/24/60/60/365)
+puts ''
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts ''
+puts 'fun with strings'
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+puts ''
+puts 2.2 * 3
+puts 2.2 * 3.0
