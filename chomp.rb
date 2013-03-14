cc475bb7aa1abcf2782b2e7a3915ae5a016226f3
Liz Abinante <eabinante@gmail.com>
2013-03-14 11:25:27 -0500

    variables, gets, and chomp

---
+++ chomp.rb
@@ -0,0 +1,7 @@
+puts 'Hi! Let\'s be friends! What is your first name?'
+nameF = gets.chomp
+puts 'Amazing. What is your middle name?'
+nameM = gets.chomp
+puts 'What is your last name?'
+nameL = gets.chomp
+puts 'Now I can steal your identity, ' + nameF + ' ' + nameM + ' ' + nameL + '! I am laughing maniacally.'
---
+++ variables.rb
@@ -0,0 +1,33 @@
+marriedName = 'Elizabeth Marie Abinante FitzGerald'
+birthDay = 'October 26'
+puts 'When I get married, my name will be ' + marriedName
+puts '... which, seriously, is way too long.'
+puts 'My birthday is ' + birthDay + '.'
+puts 'And I\'m also getting married on ' + birthDay + ' because I really like presents.'
+puts ''
+
+row = 'Row'
+puts row + 's 1-2: k all sts.'
+puts 'Repeat ' + row + ' 2 a total of 9 times more.'
+puts 'Next ' + row + ': p all sts.'
+puts ''
+
+var1 = 2
+var2 = '7'
+puts var1.to_s + var2
+puts var1 + var2.to_i
+puts ''
+
+puts 'Let\'s do some conversions.'
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts ''
+
+puts '5 is my favorite number!'.to_i
+puts 'Who asked you about 5 or whatever?'.to_i
+puts 'Your momma did.'.to_f
+puts ''
+puts 'stringy'.to_s
+puts 3.to_i
+puts ''
