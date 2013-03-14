f3319865ba0d5b81d0d95d8e0e4f0dc08e6006ff
Liz Abinante <eabinante@gmail.com>
2013-03-14 11:34:43 -0500

    favorite number

--- chomp.rb
+++ chomp.rb
@@ -5,3 +5,9 @@ nameM = gets.chomp
 puts 'What is your last name?'
 nameL = gets.chomp
 puts 'Now I can steal your identity, ' + nameF + ' ' + nameM + ' ' + nameL + '! I am laughing maniacally.'
+puts ''
+
+puts 'What is your favorite number?'
+faveN = gets.chomp
+faveNu = faveN.to_i + 1
+puts 'Really? My favorite number is ' + faveNu.to_s + ', which I think you can agree is much better.'
