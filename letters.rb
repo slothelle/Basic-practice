79898c66c3b28ad6faf3bf1a580e97aba16b2cf9
Liz Abinante <eabinante@gmail.com>
2013-03-14 10:37:50 -0500

    adding some arithmetic in my strings

--- letters.rb
+++ letters.rb
@@ -2,3 +2,8 @@ puts 'Hello, my name is Liz.'
 puts ''
 puts 'I am writing lots of basic code while I learn.'
 puts 'This is all from Learn How to Program by Chris Pine.'
+puts ''
+puts 'knit ' * 4
+puts 'Extrapolating... can I use arithmetic? Probably. It is smart.'
+puts 'I ' + 'really ' * 3 + 'love wine.'
+puts 'Damn spaces.'
