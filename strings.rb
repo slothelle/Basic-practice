19e7d8f7758b6e905ee618963634e988c97799c3
Liz Abinante <eabinante@gmail.com>
2013-03-17 10:23:12 -0500

    more strings

--- strings.rb
+++ strings.rb
@@ -1,6 +1,8 @@
 #practice from ruby docs
 #http://www.ruby-doc.org/core-1.9.3/String.html
 
+#always using to puts to display result in console 
+
 # str * integer 
 puts "Cheese! " * 3
 
@@ -55,4 +57,57 @@ puts b[/[aeiou](.)\3/]
 
 puts b["bing"]
 puts b["bang"]
+
+#bytesize of the str
+#how is this different than length?
+puts "what a lovely bunch of coconuts".bytesize 
+puts "what a lovely bunch of coconuts".length
+
+#byteslice returns a substring
+puts "what a lovely bunch of coconuts".byteslice(1, 4)
+puts "what a lovely bunch of coconuts".byteslice(1..8)
+
+#capitalize, with bang
+#important to note that ! permanently changes the str
+#if you try to use ! after already changing the str, you get nil
+c = "alphabetically speaking"
+puts c.capitalize
+puts c
+puts c.capitalize!
+puts c
+
+#casecmp similar to str comparison, but is not case sensitive
+puts "hello lala".casecmp("hello la")
+puts "hello lala".casecmp("Hello LALA")
+puts "hello lala".casecmp("hello lalala")
+
+#center
+#hey! something i already know! hooray!
+puts "I would like some bacon!".center(100)
+
+#passes each character in str to the given block
+#what would this be useful for?
+puts "abcdefghijk".each_char {|c| print c, ' '}
+#ruby docs show this as "a b c d e f g h i j k", 
+#but my modified str produces that + the original str?
+
+#chomp v. chop?
+#removing separator v. removing last character
+
+#one-character str from the beginning of str
+#clearing out the str
+#what would these be used for?
+puts c.chr
+puts c.clear
+
+#counting characters in str
+#starting with a capital letter produces different results
+d = "itsy bitsy spider went up"
+puts d.count "spid"
+puts d.count " wen"
+puts d.count "bit"
+
+
+
+
 
