a133631874cb37e98914c7941804bd20f17f1fcc
Liz Abinante <eabinante@gmail.com>
2013-03-19 08:20:16 -0500

    combined assignment operatiors

---
+++ combined.rb
@@ -0,0 +1,31 @@
+#combined assignment operators
+num = 5
+num1  = 5
+num2 = 10
+result = num1 + num2
+puts num
+puts " "
+
+#multiply the current value of num5 and store the new value
+num = num * 4
+puts num
+
+#or add 1 to the current value result3 and store that new value
+result = result + 1
+puts result
+
+#these expressions are very common, so there are shortcuts in Ruby
+num *= 4
+puts num
+
+result += 1
+puts result
+
+num -= 4
+puts num
+
+result /= 3
+puts result
+
+result %= 2
+puts result 
--- variables2.rb
+++ variables2.rb
@@ -1,15 +1,15 @@
 #practice from dbc socrates
 num = 5
 puts num
-puts "---"
+puts " "
 
 name = "Josh"
 puts name
-puts "---"
+puts " "
 
 programming_language = "Ruby"
 puts programming_language
-puts "---"
+puts " "
 
 num1  = 5
 num2 = 10
@@ -17,7 +17,7 @@ result = num1 + num2
 #adding two variables together to produce a new variable
 
 puts result
-puts "---"
+puts " "
 
 #gets.chomp
 puts "Enter your name"
@@ -31,11 +31,31 @@ puts "Great, enter another one"
 num4 = gets.chomp
 result2 = num3 + num4
 puts "The sum of #{num3} and #{num4} is #{result2}."
-puts "---"
+puts " "
 
 #but you can change it to be an integer
 result2 = num3.to_i + num4.to_i
 puts "The sum of #{num3} and #{num4} is #{result2}."
+puts " "
 
 #using #{variable} allows you to put Ruby code or a variable 
 #into a str without a bunch of concatenation 
+
+#combined assignment operators
+num = 5
+puts num
+puts " "
+
+#multiply the current value of num5 and store the new value
+num = num * 4
+puts num
+
+#or add 1 to the current value result3 and store that new value
+result = result + 1
+puts result
+
+#these expressions are very common, so there are shortcuts in Ruby
+num *= 4
+puts num
+result += 1
+puts result
