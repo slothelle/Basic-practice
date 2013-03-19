27c5ee5109b5cbd6ac20cb49b8c714bf3ecafed4
Liz Abinante <eabinante@gmail.com>
2013-03-19 08:25:42 -0500

    new

---
+++ varvocab.rb
@@ -0,0 +1,20 @@
+#variable vocabulary
+#socrates dbc
+
+first_name = "Liz"
+last_name = "Abinante"
+full_name = "#{first_name} #{last_name}"
+puts full_name
+
+num1 = 10
+num2 = 45
+
+sum = num1 + num2
+puts sum
+
+sum += 5
+puts sum
+
+#variables must begin with a lowercase letter (a-z), or an underscore
+#conventionally never contain uppercase letters (but they can)
+#breaks are replaced with underscores
