7b6f844f93edc33b791f1ef32af4b9b0bd25e8f3
Liz Abinante <eabinante@gmail.com>
2013-03-21 10:24:54 -0500

    no expressions

--- conditional.rb
+++ conditional.rb
@@ -4,10 +4,36 @@ puts 3 <= 3
 puts 'cat' > 'dog'
 puts 'cat' == 'CAT'
 puts (2+1) >= 7
-
+#
 #determines inequality 
 puts 3 != 7
-
+#
 #common mistake to use = as comparison
 #= is for assignment
 #== is for comparison
+#
+#some conditionals have no expressions
+num1 = 5
+num2 = 5
+puts num1.eql?(num2)
+#things with a ? return a true/false statement by comparing against argument
+#
+#both of these should return true
+word = "hello"
+puts word.start_with?("h") || word.end_with?("o")
+#
+#conditional statements can also be formed without an explicit statement 
+#examples
+if "hello"
+  puts "hey"
+end
+#
+num = 5
+if num
+  puts "five"
+end
+#ruby has truthy/falsy 
+#if any value other than nil or false is the result
+#it will output true
+#nil means that absolutely nothing exists
+#it's not the same as 0, or " "
