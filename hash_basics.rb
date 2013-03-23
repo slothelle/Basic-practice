513eaae0fed68114767d7252787222f9b5a869bd
Liz <eabinante@gmail.com>
2013-03-23 17:50:28 -0500

    Create hash_basics.rb

---
+++ hash_basics.rb
@@ -0,0 +1,11 @@
+#hash basics
+#elements of a hash are identified by name, not position (essentially unordered lists)
+#don't understand "first element", but they do understand "element named Frank"
+#
+#
+#syntax: variable {value}
+#
+#example
+person = {:first_name => 'Frank', :last_name => 'Hardy', :age => 25}
+#symbols :first_name, :last_name, :age are called keys
+#access specific elements of a hash through their keys
