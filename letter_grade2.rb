1b7534012166046675fc58e189301c83974f6cda
Liz Abinante <eabinante@gmail.com>
2013-03-25 12:21:24 -0500

    new

---
+++ letter_grade2.rb
@@ -0,0 +1,5 @@
+#Exercise: Calculate the letter grade of a series of grades
+#Create a method get_grade that accepts an Array of test scores. Each score in the array should be between 0 and 100, where 100 is the max score.
+#Compute the average score and return the letter grade as a String, i.e., 'A', 'B', 'C', 'D', 'E', or 'F'.
+#
+#stuck on this :(
