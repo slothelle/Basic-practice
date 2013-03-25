fd1f2fa03a244da606ec032205389446ad3db736
Liz Abinante <eabinante@gmail.com>
2013-03-25 13:22:02 -0500

    modulu

--- practice_knithat.rb
+++ practice_knithat.rb
@@ -5,7 +5,7 @@
 #converting from inches to cm if needed
 #validating inputs and handling invalid inputs
 #selection of brim pattern to generate custom multiples
-
+#
 puts 'How about we knit a hat?'
 puts 'I think that\'s a good idea.'
 puts 'But I need some information from you first.'
@@ -28,3 +28,17 @@ puts 'Using a 16 inch circular needle, CO ' + cO.to_s + ' sts.'
 puts 'PM and join for knitting in the round.'
 puts 'Work hat band in chosen motif (k2, p2 ribbing, for example) for approximately 2 inches.'
 puts 'For the hat body, k all sts for every round until hat measures 2" from the top of your head.'
+#
+#
+#
+#
+#example of using my own method and modulu to determine multiple
+def cast_on(gauge_inch, circumference)
+  if gauge_inch * circumference % 4 == 0
+    return "Hooray, have a multiple of 4."
+  else 
+    return "Sorry, you need a multiple of 4."
+  end
+end
+
+puts cast_on(6, 24)
