f8e8ee543e62c1ca162988f7965fa530cf0dcbc8
Liz <eabinante@gmail.com>
2013-04-05 07:37:20 -0500

    new

---
+++ exercises_ruby_the_hard_way/4_variables_names.rb
@@ -0,0 +1,18 @@
+# Variables name are there to help code read more like English
+# and to help us remember when we revisit code what it is we're looking at
+
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+puts "There are #{cars} cars available."
+puts "There are only #{drivers} drivers available."
+puts "There will be #{cars_not_driven} empty cars today."
+puts "We can transport #{carpool_capacity} people today."
+puts "We have #{passengers} passengers to carpool today."
+puts "We need to put about #{average_passengers_per_car} in each car."
