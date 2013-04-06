6264c17a868f8b8814d099303facb425676df4ae
Liz Abinante <eabinante@gmail.com>
2013-04-05 19:32:13 -0500

    new new new

---
+++ exercises_ruby_the_hard_way/7_more_printing.rb
@@ -0,0 +1,21 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as %s." % 'snow'
+puts "And everywhere that Mary went."
+puts "." * 10
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5
+print end6 + end7 + end8 + end9 + end10
+print end11 + end12
---
+++ exercises_ruby_the_hard_way/9_printing.rb
@@ -0,0 +1,15 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJuly\nAug"
+
+puts "Here are the days: ", days
+puts "Here are the months: ", months
+
+puts <<PARAGRAPH
+There's something going on here. 
+With the PARAGRAPH thing
+We'll be able to type as much as we like.
+Even 4 lines if we want.
+Or 5.
+Or 6.
+Or more.
+PARAGRAPH
