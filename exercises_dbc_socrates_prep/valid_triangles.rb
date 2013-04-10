03d816320ab16f43d7f4fbc0c351ccbee4792bb9
Liz Abinante <eabinante@gmail.com>
2013-04-10 07:21:05 -0500

    notes

--- exercises_dbc_socrates_prep/valid_triangles.rb
+++ exercises_dbc_socrates_prep/valid_triangles.rb
@@ -1,8 +1,12 @@
-#Exercise: Triangle side lengths
-#Write a method valid_triangle? which takes as its input three non-negative numbers. It should return true if the three numbers could form the side lengths of a triangle and false otherwise.
-#
-#The arguments don't correspond to specific sides. Don't worry about handling negative inputs — garbage in, garbage out.
-#
+# Exercise: Triangle side lengths
+# Write a method valid_triangle? which takes as its
+# input three non-negative numbers.
+# It should return true if the three numbers could form
+# the side lengths of a triangle and false otherwise.
+
+#The arguments don't correspond to specific sides.
+# Don't worry about handling negative inputs.
+
 def valid_triangle?(a, b, c)
   if (a == 0 || b == 0 || c == 0)
     return false
@@ -18,7 +22,13 @@ def valid_triangle?(a, b, c)
     return false
   end
 end
-#had a really hard time with this even after brushing up on geometry
-#definitely not my strong suit, and I know this code could be cleaner
-#can't tell if the code isn't clean because I don't understand the math, or because I don't get the code
-#itemized every option in above code: 0 length sides, equilateral, right, impossible, triples
+
+# had a really hard time with this even
+# after brushing up on geometry
+# definitely not my strong suit
+# I know this code could be cleaner
+# can't tell if the code isn't clean 
+# because I don't understand the math,
+# or because I don't get the code
+# itemized every option in above code: 
+# 0 length sides, equilateral, right, impossible, triples
