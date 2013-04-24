8207ec08e42b6d76c607efccfeb5c8c11c3f87f4
Liz Abinante <eabinante@gmail.com>
2013-04-24 12:09:36 -0500

    new

---
+++ notes/array_samples/transpose.rb
@@ -0,0 +1,11 @@
+arr = [["hello", "goodbye"], ["pickles", "cheese"], ["bonjour", "ciao"]]
+arr.transpose
+
+# Returns
+# [["hello", "pickles", "bonjour"], ["goodbye", "cheese", "ciao"]]
+
+ary = [["a", "b", "c"], [1, 2, 3], ["apples", "oranges"]]
+ary.transpose
+
+# Hideous error:
+# (eval):190: (eval):190:in `transpose': element size differs (2 should be 3) (IndexError) from (eval):190
