b7b1cfabdbd56b2f8f171381908a6bc0192e8fe6
Liz Abinante <eabinante@gmail.com>
2013-05-02 15:52:12 -0500

    ()

--- discount_calculator.rb
+++ discount_calculator.rb
@@ -11,7 +11,7 @@ puts how_much_will_i_save(10, 120.05)
 # Returns only how much someone will pay.
 
 def how_much_will_this_cost(percentage_off, retail_price)
-  you_pay = (retail_price) - ((retail_price * percentage_off) / 100)
+  you_pay = retail_price - ((retail_price * percentage_off) / 100)
   return "$#{retail_price} at #{percentage_off}% means you'll pay $#{you_pay.round(2)}."
 end 
 
