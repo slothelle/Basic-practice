384689d6e033e1d0a434946619eb0a694daa941b
Liz Abinante <eabinante@gmail.com>
2013-05-03 11:06:52 -0500

    new

---
+++ puppy.rb
@@ -0,0 +1,89 @@
+class PuppyGotchi
+  def initialize(name)
+    @name = name 
+    @puppy_belly = 10
+    @puppy_bladder = 0
+    @puppy_bowels = 0
+    @puppy_awake = true
+  end 
+
+  def feed_puppy
+    if @puppy_belly == 10
+      full_puppy
+    else 
+      @puppy_belly = @puppy_belly + 1
+    end 
+  end
+
+  def water_puppy
+  end 
+
+  def play_with_puppy
+  end
+
+  def sick_puppy
+  end
+
+  def dead_puppy
+  end
+
+  def puppy_sleeps
+    puts "#{name} is sleeping."
+    @puppy_awake = false 
+
+    3.times do 
+      if @puppy_awake = false
+        passing_time
+      end 
+      if @puppy_awake
+        passing_time
+      end
+    end 
+    
+  end 
+
+  private
+  def full_puppy
+    puts "#{name} is full and doesn't want any food."
+    puppy_sleeps
+  end 
+
+  def puppy_hungry?
+    @puppy_belly <= 1
+  end 
+
+  def puppy_tinkle?
+    @puppy_bladder >= 8
+  end
+
+  def puppy_poopy?
+    @puppy_bowels >= 8
+  end
+
+  def passing_time
+    if @puppy_belly >= 2
+      @puppy_belly = @puppy_belly - 1
+      @puppy_bowels = @puppy_bowels + 1
+      @puppy_bladder = @puppy_bladder + 1
+    elsif @puppy_belly == 1
+      # Wake up puppy and demand to be fed
+    else 
+      # Puppy is starving
+      # Puppy doesn't wake up, it's dead
+    end 
+
+    if @puppy_bowels == 10 || @puppy_bladder == 10
+      # Puppy has accident 
+    end
+
+    if puppy_hungry?
+      # Puppy wakes up and demands to be fed
+    end 
+
+    if puppy_poopy? || puppy_tinkle?
+      # Puppy wakes up and demands to go potty
+    end 
+
+  end 
+
+end
