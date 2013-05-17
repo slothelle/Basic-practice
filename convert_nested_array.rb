3eced2f100543aaa0cafa5ad5a5db08529cf88ad
Liz Abinante <eabinante@gmail.com>
2013-05-17 18:14:33 -0500

    cleaning

--- convert_nested_array.rb
+++
@@ -1,6 +0,0 @@
-array = [["Number", "Name", "Position", "Points per Game"],
- ["12","Joe Schmo","Center",[14, 32, 7, 0, 23] ],
- ["9", "Ms. Buckets ", "Point Guard ", [19, 0, 11, 22, 0] ],
- ["31", "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25] ], 
- ["18", "Sally Talls", "Power Forward ", [18, 29, 26, 31, 19] ], 
- ["22", "MK DiBoux ", "Small Forward ", [11, 0, 23, 17, 0] ]]
